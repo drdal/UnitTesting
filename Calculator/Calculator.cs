d083d161025da59382a756a12f13cf249f6fe88c
drdal <dal@dr3.dk>
2022-05-16 12:33:17 +0200

    Created Calculator library

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,22 @@
+﻿namespace Calculator;
+
+public static class Calculator
+{
+    public static double Add(double X, double Y)
+    {
+        return X + Y;
+    }
+
+    public static double Divide(double X, double Y)
+    {
+        if (Y != 0)
+        {
+            return X / Y;
+        }
+        else {
+            throw new DivideByZeroException();
+        }
+    }
+
+}
+
